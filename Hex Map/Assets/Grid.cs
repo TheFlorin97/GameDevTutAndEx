846bed004c3bd3c41349c81b0fc90262fa00cd38
TheFlorin97 <constantin.florin97@yahoo.com>
2021-07-13 17:15:44 +0300

    Grid

---
+++ Hex Map/Assets/Grid.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+
+public class Grid : MonoBehaviour
+{
+    
+    public int xSize, ySize;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
